829f151f86f41111e720c8ce1f53709a8772ee8d
Ankit Bhardwaj <ankit.bhardwaj@v2solutions.com>
2016-03-11 11:27:26 +0530

    Resolved again

--- BBFCouplesViewController.swift
+++ BBFCouplesViewController.swift
@@ -34,24 +34,12 @@ class BBFCouplesViewController: UIViewController {
               // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func boysExerciseViewController(sender: AnyObject) {
-       
-        self.performSegueWithIdentifier("BoyViewController", sender: self)
-    }
-    
-    @IBAction func girlsExerciseViewController(sender: AnyObject) {
-       self.performSegueWithIdentifier("GirlViewController", sender: self)
-    }
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+//    @IBAction func boysExerciseViewController(sender: AnyObject) {
+//    self.performSegueWithIdentifier("BoyViewController", sender: self)
+//    }
+//    
+//    @IBAction func girlsExerciseViewController(sender: AnyObject) {
+//       self.performSegueWithIdentifier("GirlViewController", sender: self)
+//    }
+ 
 }
